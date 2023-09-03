47322a8ddc87c3cb0ba47c1f1f102808eefe75eb
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-09-03 20:04:48 -0300

    Atualização do sistema de login

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Permitir que qualquer origem acesse o servidor (para desenvolvimento)
+header("Access-Control-Allow-Methods: POST"); // Permitir apenas métodos POST
+header("Access-Control-Allow-Headers: Content-Type"); // Permitir apenas o cabeçalho Content-Type
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "login";
+
+$conexao = new mysqli($servername, $username, $password, $dbName);
+
+if ($conexao->connect_error) {
+    die("Conexão falhou: " . $conexao->connect_error);
+}
+
+$data = json_decode(file_get_contents("php://input"));
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = $conexao->real_escape_string($data->email);
+    $senha = $data->senha;
+
+    // Consulta SQL para verificar as credenciais do usuário
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $result = $conexao->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $senhaHash = $row["senha"];
+
+       
+        if (password_verify($senha, $senhaHash)) {
+
+            $response = [
+                "id" => $row["id"],
+                "nome" => $row["nome"],
+                "email" => $row["email"],
+            ];
+            echo json_encode($response);
+        } else {
+            echo "Credenciais inválidas";
+        }
+    } else {
+        echo "Usuário não encontrado";
+    }
+}
+
+?>
